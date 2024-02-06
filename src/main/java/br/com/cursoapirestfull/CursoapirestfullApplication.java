7b8b1bc6a3878d10524a8298d1c6de5e67d4994b
Cleiton <cleiton18ads@gmail.com>
2024-02-06 11:30:26 -0300

    Primeiro Commit Curso Api Restfull

---
+++ src/main/java/br/com/cursoapirestfull/CursoapirestfullApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursoapirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoapirestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoapirestfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cursoapirestfull/CursoapirestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursoapirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoapirestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
